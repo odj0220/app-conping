28ba1d7186ebd5ae7b41d719e4b29c9ae7cc0e59
odj0220 <odj0220@nate.com>
2022-08-05 00:34:52 +0900

    storage get,set 구현

--- app/src/main/java/com/dj/conping/WebAppInterface.kt
+++ app/src/main/java/com/dj/conping/WebAppInterface.kt
@@ -14,9 +14,9 @@ var CALLBACK_FUNCTION = "conpingCallback"
 class WebAppInterface// Instantiate the interface and set the context
 internal constructor(internal var mContext: Context, var webView: WebView) {
 
-    fun callBack(eventId: String, data: Any) {
+    fun callBack(eventId: String, data: Any = "") {
         webView.post(Runnable {
-            webView.loadUrl("javascript:" + CALLBACK_FUNCTION + "('" + eventId + "'," + data + ")")
+            webView.loadUrl("javascript:" + CALLBACK_FUNCTION + "('" + eventId + "','" + data + "')")
         })
     }
 
@@ -56,7 +56,7 @@ internal constructor(internal var mContext: Context, var webView: WebView) {
         alert.setNegativeButton("취소", object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
                 Log.d("Confirm", "negative")
-                callBack(eventId, false);
+                callBack(eventId);
             }
         } ).setCancelable(false)
         alert.setPositiveButton("확인", object : DialogInterface.OnClickListener{
@@ -67,6 +67,30 @@ internal constructor(internal var mContext: Context, var webView: WebView) {
         })
         alert.create()
         alert.show()
+    }
+
+    @JavascriptInterface
+    fun setStorage(map: String) {
+        val map: JSONObject = JSONObject(map)
+        val sharedPref = mContext.getSharedPreferences("conping", Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putString(map.getString("key"), map.getString("value"))
+            apply()
+        }
+
+    }
+
+    @JavascriptInterface
+    fun getStorage(key: String, eventId: String) {
+        val sharedPref = mContext.getSharedPreferences("conping", Context.MODE_PRIVATE) ?: return
+        val defaultValue = ""
+        val result = sharedPref.getString(key, defaultValue);
+        if (result != null) {
+            Log.d("storage-get", result)
+        }
+        if (result != null) {
+            callBack(eventId, result)
+        };
     }
 
 }
